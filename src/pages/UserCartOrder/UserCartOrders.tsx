270c090f9cad460843110f5244248c94c3637d96
Gramnaster <jocvillalon@yahoo.com.ph>
2025-11-03 02:05:17 +0800

    Fix: Adjusts time to include hour minute second

--- src/pages/UserCartOrder/UserCartOrders.tsx
+++ src/pages/UserCartOrder/UserCartOrders.tsx
@@ -174,10 +174,13 @@ const UserCartOrders = () => {
   });
 
   const formatDate = (dateString: string) => {
-    return new Date(dateString).toLocaleDateString('en-US', {
+    return new Date(dateString).toLocaleString('en-US', {
       year: 'numeric',
       month: '2-digit',
       day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
     });
   };
 
